ae6eb188f6360a1af4d2508c8fdf840878284e4d
Saba-Var <vartasashvili94@gmail.com>
2022-12-10 12:08:33 +0400

    feat: draw 3 segments and create game screen file

---
+++ game_screen.py
@@ -0,0 +1,6 @@
+from turtle import Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Snake Game 🐍")
+screen.setup(height=600, width=600)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from turtle import Screen
+from game_screen import screen
+from turtle import Turtle
 
-screen = Screen()
-screen.bgcolor("black")
-screen.title("Snake Game 🐍")
-screen.setup(height=600, width=600)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for starting_position in starting_positions:
+    segment = Turtle("square")
+    segment.color("white")
+    segment.goto(starting_position)
 
 screen.exitonclick()
